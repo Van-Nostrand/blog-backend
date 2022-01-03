8035ab94e081cf4e88d63fd734356ce020eb09fd
Van-Nostrand <dmdoull43@gmail.com>
2022-01-02 22:20:26 -0800

    edit route works

--- handlers/posts.js
+++ handlers/posts.js
@@ -4,7 +4,7 @@ exports.getAllPosts = async function (req, res, next) {
   try {
     const allPosts = await db.BlogPost.find()
     if (!allPosts) throw new Error('error getting blog posts')
-    return res.render('blog', {allPosts})
+    return res.render('blog', { allPosts })
   } catch(err) {
     console.error(err)
   }
@@ -12,9 +12,25 @@ exports.getAllPosts = async function (req, res, next) {
 
 exports.createPost = async function (req, res, next) {
   try {
-    const post = await db.BlogPost.create({...req.body})
-    return res.render('blog', {allPosts: [post]})
+    const post = await db.BlogPost.create({ ...req.body })
+    return res.render('blog', { allPosts: [post] })
   } catch(err) {
     return next(err)
+  }
+}
+
+exports.editPost = async function (req, res, next) {
+  try {
+    // console.log('looking for a post', req.params)
+    const post = await db.BlogPost.findById(req.params.id)
+    // console.log('found a post!!', post)
+    if (post) {
+      // console.log('post exists')
+      // return res.redirect(307, '/editBlog', { post })
+      return res.render('editBlog', { post })
+    }
+    else throw new Error('Error retrieving post in editPost:')
+  } catch (err) {
+    console.error(err)
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,24 @@
 const express = require('express')
-const { getAllPosts, createPost } = require('../handlers/posts')
+const { getAllPosts, createPost, editPost } = require('../handlers/posts')
 const router = express.Router({
   mergeParams: true // what does this do?
 })
 const fakePosts = require('../constants/temp-data').fakePosts
-// const HANDLERS = require('../handlers/auth')
 
-router.route('/').get((req, res) => {
-  res.render('home')
-})
+router.route('/')
+  .get((req, res) => res.render('home'))
 
-// router.route('/blog').get((req, res) => {
-//   // res.render('blog', {data: 'a string being passed in'})
-//   res.render('blog', {fakePosts})
-
-// })
-router.route('/blog').get(getAllPosts)
+router.route('/blog')
+  .get(getAllPosts)
 
 router.route('/createpost')
-  .get((req, res) => {
-    res.render('createpost')
-  })
+  .get((req, res) => res.render('createpost'))
   .post(createPost)
-  // .post((req, res, next) => {
-  //   console.log('request!', req.body)
-  //   // console.log('request!', Object.keys())
-  //   res.render('createpost')
-  // })
-router.route('/api/test').post((req, res) => {
-  res.send('received post request with data' + req.body)
-})
+
+router.route('/editpost/:id')
+  .get(editPost)
+
+router.route('/api/test')
+  .post((req, res) => res.send('received post request with data' + req.body))
+
 module.exports = router
