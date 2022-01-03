64e237ad53d8b6d60c38301574c0eb233fda8d4b
Van-Nostrand <dmdoull43@gmail.com>
2022-01-02 22:50:08 -0800

    some template edits. edit vs view blog

--- handlers/posts.js
+++ handlers/posts.js
@@ -3,7 +3,7 @@ const db = require('../models')
 exports.getAllPosts = async function (req, res, next) {
   try {
     const allPosts = await db.BlogPost.find()
-    if (!allPosts) throw new Error('error getting blog posts')
+    if (!allPosts) throw new Error('error getting blog posts: ')
     return res.render('blog', { allPosts })
   } catch(err) {
     console.error(err)
@@ -13,6 +13,7 @@ exports.getAllPosts = async function (req, res, next) {
 exports.createPost = async function (req, res, next) {
   try {
     const post = await db.BlogPost.create({ ...req.body })
+    if (!post) throw new Error('error creating a post: ')
     return res.render('blog', { allPosts: [post] })
   } catch(err) {
     return next(err)
@@ -21,15 +22,9 @@ exports.createPost = async function (req, res, next) {
 
 exports.editPost = async function (req, res, next) {
   try {
-    // console.log('looking for a post', req.params)
     const post = await db.BlogPost.findById(req.params.id)
-    // console.log('found a post!!', post)
-    if (post) {
-      // console.log('post exists')
-      // return res.redirect(307, '/editBlog', { post })
-      return res.render('editBlog', { post })
-    }
-    else throw new Error('Error retrieving post in editPost:')
+    if (!post) throw new Error('Error retrieving post in editPost:')
+    return res.render('editBlog', { post, editMode: true })
   } catch (err) {
     console.error(err)
   }
