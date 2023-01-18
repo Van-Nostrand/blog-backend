f7dd3036464b6d1f4bc54ef388030fc0bd8803c5
Van-Nostrand <dmdoull43@gmail.com>
2023-01-18 10:56:21 -0800

    backing up. not sure what i was doing

--- handlers/posts.js
+++ handlers/posts.js
@@ -14,6 +14,7 @@ exports.createPost = async function (req, res, next) {
   try {
     const post = await db.BlogPost.create({ ...req.body })
     if (!post) throw new Error('error creating a post: ')
+    console.log('post is ', post)
     return res.render('blog', { allPosts: [post] })
   } catch(err) {
     return next(err)
@@ -24,7 +25,8 @@ exports.editPost = async function (req, res, next) {
   try {
     const post = await db.BlogPost.findById(req.params.id)
     if (!post) throw new Error('Error retrieving post in editPost:')
-    return res.render('editBlog', { post, editMode: true })
+    // else return res.send(JSON.stringify(post))
+    else return res.render('editBlog', { post, editMode: true })
   } catch (err) {
     console.error(err)
   }
